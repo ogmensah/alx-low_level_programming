e1ccd484aaf5fe7fca3256d2dc2b37d231baf43e
gom6s <gmensah025@gmail.com>
2022-09-08 22:08:18 +0000

    scripts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - this function runs first before any other
+(*
+ * Return: 0 if program runs successfully
+ */
 int main(void)
 {i
 	puts("\"Programming is like building a multilingual puzzle");
