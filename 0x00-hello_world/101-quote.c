3a361d89de983956278a00d0e2fac49ba3a8e134
ogmensah <63637354+ogmensah@users.noreply.github.com>
2022-09-09 01:04:30 +0000

    Update 101-quote.c
    scripts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,19 @@
-#include <stdio.h> 
- 
-int write(int filedes, const char *buf, unsigned int nbyte); 
-int main( int argc, char** argv ) 
-{ 
-   write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60); 
- 
-   return 0; 
+#include <stdio.h>
+
+main() 
+
+{
+
+char st[57] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
+
+	int n=0; 
+
+	while(st[n] != '\0') { 
+
+		putchar(st[n]); 
+
+		n++; 
+
+	} 
+
 } 
