07a87852f2c13953e0818192dc7ccfcbbcb1e9d6
gom6s <gmensah025@gmail.com>
2022-09-15 12:05:58 +0000

    scripts

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * mul - it checks for upper case letters
+ *@a: first parameter
+ *@b: second parameter
+ * Return: returns the product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
