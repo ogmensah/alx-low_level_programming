818612aced968ea4618c6953a0564061e73a3ff3
gom6s <gmensah025@gmail.com>
2022-09-15 11:48:22 +0000

    scripts

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#ifndef _PUTCHAR
+#define _PUTCHAR
+/**
+ *_putchar - this is a prototype for a function used in printing a character at a time
+ *print_alphabet - this prints the alphabets from a to z
+ *print_alphabet_x10 - this prints the alphabets from a to z 10 times
+ *_islower - checks for lower letters
+ *_isalpha - checks for alphabet
+ */
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+
+#endif /* _PUTCHAR */
