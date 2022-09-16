65afbcbe34d28f407c53c499b4c7e707039b0905
gom6s <gmensah025@gmail.com>
2022-09-16 22:56:12 +0000

    script

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,22 +9,22 @@
 
 void print_triangle(int size)
 {
-        int i, j;
+	int i, j;
 
-        for (i = 0; i < size; i++)
-        {
-                for (j = 0; j < (size - i + 1); j++)
-                {
-                        _putchar(32);
-                }
-                for (j = 0; j < (i + 1); j++)
-                {
-                        _putchar(35);
-                }
-                _putchar('\n');
-        }
-        if (size <= 0)
-        {
-                _putchar('\n');
-        }
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < (size - i + 1); j++)
+		{
+			_putchar(32);
+		}
+		for (j = 0; j < (i + 1); j++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
 }
