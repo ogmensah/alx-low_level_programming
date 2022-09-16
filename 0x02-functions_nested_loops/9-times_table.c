1ee289cc7c0311129c5937eab0191ef5cf2a927f
gom6s <gmensah025@gmail.com>
2022-09-16 17:22:52 +0000

    scripts

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * times_table - this invokes the _puchar function to print characters
+ *
+ * Return: returns 1 when successful
+ */
+
+void times_table(void)
+{
+	for(i=0;i<11;i++){
+	    for(j=1;j<10;j++){
+	        if(i==0 && j==1){
+		    _putchar(32);
+		    _putchar(32);
+		}
+		
+		if(j==1 && i!=0)
+		{
+
+	       	}
+	    }
+	}
+
+
+}
