0b66263eac693dd3c2d410537305103b2f35d389
gom6s <gmensah025@gmail.com>
2022-09-16 19:01:46 +0000

    script

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,13 +11,21 @@ void print_diagonal(int n)
 {
 	int a = 1;
 	int b;
-        for (; a <= n  ; a++)
+
+	if (n <= 0)
 	{
-		for (b = 0; b < a; b++)
-        	{
-                	_putchar(32);
-        	}
-	_putchar(92);
-        _putchar('\n');
+		_putchar('\n');
+	}
+	else
+	{
+        	for (; a <= n  ; a++)
+		{
+			for (b = 0; b < a; b++)
+        		{
+                		_putchar(32);
+        		}
+			_putchar(92);
+        		_putchar('\n');
+		}
 	}
 }
