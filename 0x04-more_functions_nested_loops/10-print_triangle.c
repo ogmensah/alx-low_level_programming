cb75db108ccfa032956334408876546aa7dd1161
gom6s <gmensah025@gmail.com>
2022-09-16 22:49:49 +0000

    script

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_triangle - prints triangle
+ * @size: the size of the triangle
+ */
+
+void print_triangle(int size)
+{
+        int i, j;
+
+        for (i = 0; i < size; i++)
+        {
+                for (j = 0; j < (size - i + 1); j++)
+                {
+                        _putchar(32);
+                }
+                for (j = 0; j < (i + 1); j++)
+                {
+                        _putchar(35);
+                }
+                _putchar('\n');
+        }
+        if (size <= 0)
+        {
+                _putchar('\n');
+        }
+}
