8ab6484136aaee6242b56e19f65ced49278215de
gom6s <gmensah025@gmail.com>
2022-09-16 18:50:59 +0000

    scripts

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_diagonal - prints diagonal line
+ * @n: specifies the length of the diagonal line
+ */
+
+void print_diagonal(int n)
+{
+	int a = 1;
+	int b;
+        for (; a <= n  ; a++)
+	{
+		for (b = 0; b < a; b++)
+        	{
+                	_putchar(32);
+        	}
+	_putchar(92);
+        _putchar('\n');
+	}
+}
