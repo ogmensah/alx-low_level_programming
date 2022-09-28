2ab486173317a885d523868b4738d45236b7f1d8
gom6s <gmensah025@gmail.com>
2022-09-28 05:46:23 +0000

    scripts

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,24 +3,23 @@
 /* more headers goes there */
 
 /**
- * _strspn - gets the length of prestring substring
- *@s: input string
- *@accept: query character
- * Return: returns pointer to found character or NULL when not found
+ * print_diagsums - gets the length of prestring substring
+ *@a: input string
+ *@size: query character
  */
 
-void print_chessboard(char (*a)[8])
+void print_diagsums(int *a, int size)
 {
-
 	int i = 0;
 	int j = 0;
 	char *b[8];
+
 	while (j < 8)
 	{
 		b = a[j];
 		while (i < 8)
 		{
-			printf("%s",b[i]);
+			printf("%s", b[i]);
 			_putchar(',');
 			_putchar(' ');
 			i++;
