a1a7396fdc21d2d3affe3e9fea8c01e240d6f1fe
gom6s <gmensah025@gmail.com>
2022-09-30 14:03:41 +0000

    C files

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * main - prints the name of the program
+ * @argc: the number of parameters passed during execusion
+ * Return: returns 0 if program runs successfully
+ */
+
+int main (int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
