cf081afa672bae977280f023221c17466e837aed
gom6s <gmensah025@gmail.com>
2022-09-30 14:25:37 +0000

    C files

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * main - prints the name of the program
+ * @argc: the number of parameters passed during execusion
+ * @argv: the list of parameters passed during execusion
+ * Return: returns 0 if program runs successfully
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
