68f25c08c336039aab6d41bcdfe2829a55f0e642
gom6s <gmensah025@gmail.com>
2022-10-01 02:51:46 +0000

    C files

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,14 +13,18 @@
 
 int main(int argc, char *argv[])
 {
-	int a = atoi(argv[1]);
+	int a = 0;
 	int c = 0;
+	int *ptr = &c;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	
+	a = atoi(argv[1]);
+	
 	if (a < 0)
 	{
 		printf("%d\n", 0);
@@ -28,24 +32,24 @@ int main(int argc, char *argv[])
 	}
 	if (a / 25 != 0)
 	{
-		c = a / 25;
+		*ptr = a / 25;
 		a = a - c * 25;
 	}
 	if (a / 10 != 0)
 	{
-		c += a / 10;
+		*ptr += a / 10;
 		a = a - (a / 10) * 10;
 	}
 	if (a / 5 != 0)
 	{
-		c += a / 5;
+		*ptr += a / 5;
 		a = a - (a / 5) * 5;
 	}
 	if (a / 2 != 0)
 	{
-		c += a / 2;
+		*ptr += a / 2;
 		a = a - (a / 2) * 2;
-		c += a;
+		*ptr += a;
 	}
 	printf("%d\n", c);
 	return (0);
