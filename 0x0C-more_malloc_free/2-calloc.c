5cae08c70b2f213ffefdd1f9feb0c30a818bc00e
gom6s <gmensah025@gmail.com>
2022-10-08 01:09:11 +0000

    Cfile on malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _calloc - allocates memory
+ *@nmemb: number of elements
+ *@size: size of each element
+ *Return: returns pointer to first byte of the memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *l;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	l = malloc(nmemb * size);
+	if (l == NULL)
+		return (NULL);
+	return (l);
+}
