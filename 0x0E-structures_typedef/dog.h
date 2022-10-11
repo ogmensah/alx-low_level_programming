bd0494d65bfcd11f5d3791d06ae0995bff29654d
gom6s <gmensah025@gmail.com>
2022-10-11 05:55:11 +0000

    c files

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * struct my_struct - Short description
+ * @a: First member
+ * @b: Second member
+ * @c: Third member
+ *
+ * Description: Longer description
+ */
+typedef struct dog
+{
+	char * name;
+	float age;
+	char * owner;
+};
