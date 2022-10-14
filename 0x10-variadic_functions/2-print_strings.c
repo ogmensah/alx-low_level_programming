7c733fcd8e2773f5a1713128a8e559e717192f4d
gom6s <gmensah025@gmail.com>
2022-10-14 04:03:34 +0000

    C files on variadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -4,13 +4,13 @@
 
 
 /**
- * print_numbers - prints all the parameters
+ * print_strings - prints all the parameters
  * @separator: delimiter of printed parameters
  * @n: argument
  * Return: returns sum of parameters
  *
  */
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
@@ -18,9 +18,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(ap, int));
+		if (va_arg(ap, char *) == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", va_arg(ap, char *));
+		}
 		if (separator != NULL && i < (n - 1))
-			printf("%s", separator);
+			printf("%c", *separator);
 	}
 	va_end(ap);
 	putchar('\n');
