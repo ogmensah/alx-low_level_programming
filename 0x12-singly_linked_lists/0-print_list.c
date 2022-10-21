7d219b75b68272436218c57ea56d2ff2465e8deb
gom6s <gmensah025@gmail.com>
2022-10-21 05:40:52 +0000

    Linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,11 +2,17 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ *print_list - traverses through linked lists
+ *@h: a struct pointer
+ *Return: returns number of nodes
+ */
 size_t print_list(const list_t *h)
 {
 	int i = 0;
 	const list_t *current_node = h;
-	while ( current_node != NULL)
+
+	while (current_node != NULL)
 	{
 		if (current_node->str != NULL)
 		{
