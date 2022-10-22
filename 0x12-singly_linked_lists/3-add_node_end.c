9c30be6edbc9dfeda492d4087c2970182f223d5d
gom6s <gmensah025@gmail.com>
2022-10-22 04:38:06 +0000

    Linked lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,18 +4,24 @@
 #include "lists.h"
 
 /**
- *add_node - adds new element to linked list
+ *add_node_end - adds new element to the end of linked list
  *@head: point to linked list
  *@str: string in the new element
  *Return: returns pointer to new element added
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
+	list_t *last_node;
 
+	last_node = *head;
+	while (last_node->next != NULL)
+	{
+		last_node = last_node->next;
+	}
 	new_node = (list_t *) malloc(sizeof(list_t));
 	new_node->str = strdup(str);
-	new_node->next = *head;
+	last_node->next = new_node;
 	*head = new_node;
 	return (*head);
 }
