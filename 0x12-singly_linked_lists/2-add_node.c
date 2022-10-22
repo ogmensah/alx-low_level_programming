d9bd3314b678cd2361335411015e91f7e4cd4421
gom6s <gmensah025@gmail.com>
2022-10-22 03:19:44 +0000

    Linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,8 +17,6 @@ list_t *add_node(list_t **head, const char *str)
 	if (!new_node)
 		return (NULL);
 	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-		return (NULL);
 	new_node->next = *head;
 	*head = new_node;
 	return (*head);
