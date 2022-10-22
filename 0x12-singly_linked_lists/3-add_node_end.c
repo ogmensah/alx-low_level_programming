49c8ef8ab3fa79271bebbed0283c16c13ab1b528
gom6s <gmensah025@gmail.com>
2022-10-22 03:15:48 +0000

    Linked lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,8 +14,6 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 
 	new_node = (list_t *) malloc(sizeof(list_t));
-	if (!new_node)
-		return (NULL);
 	new_node->str = strdup(str);
 	new_node->next = *head;
 	*head = new_node;
