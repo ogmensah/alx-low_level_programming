93efdca60d47f162b73ba51fb22aaa99ba0b775a
gom6s <gmensah025@gmail.com>
2022-10-24 10:37:17 +0000

    C filessingly linked lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *print_list - traverses through linked lists
+ *@h: a struct pointer
+ *Return: returns number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned long i = 0;
+	const list_t *current_node;
+
+	if (h == NULL)
+		return (0);
+
+	current_node = h;
+	while (current_node)
+	{
+		if (current_node->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("%s\n", current_node->str);
+			current_node = current_node->next;
+		}
+		i++;
+	}
+	return (i);
+}
