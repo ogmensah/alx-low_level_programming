05e128fd7f36c370259cc64a8870559087a31324
gom6s <gmensah025@gmail.com>
2022-10-25 07:26:32 +0000

    C files singly linked lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *free_listint2 - deletes list and sets head to NULL
+ *@head: a struct pointer
+ *Return: returns nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = *head;
+
+	if (*head == NULL)
+		return;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
+}
