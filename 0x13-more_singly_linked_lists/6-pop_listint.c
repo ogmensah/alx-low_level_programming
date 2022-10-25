92bf92e001dd9aa46ea8a9e6322b8006d5006a3b
gom6s <gmensah025@gmail.com>
2022-10-25 10:11:54 +0000

    C files singly linked lists

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *pop_listnint - removes head node of linked list
+ *@head: a struct pointer
+ *Return: returns head node's data.
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp = *head;
+	int u;
+
+	if (*head == NULL)
+		return (0);
+	u = temp->n;
+	return (u);
+}
