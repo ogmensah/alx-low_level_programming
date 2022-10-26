9c1e476e4eed57842b7cc326dbb88bf386d190e6
gom6s <gmensah025@gmail.com>
2022-10-26 05:10:52 +0000

    C files singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,10 +13,7 @@ size_t print_listint(const listint_t *h)
 	const listint_t *current_node;
 
 	if (h == NULL)
-	{
-		putchar('\n');
 		return (0);
-	}
 	current_node = h;
 	while (current_node != NULL)
 	{
