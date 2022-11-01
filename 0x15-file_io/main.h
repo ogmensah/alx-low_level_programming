97f3e0ef68367a95d0ced0d6af9d25dd96e8c3a0
gom6s <gmensah025@gmail.com>
2022-11-01 05:17:41 +0000

    File input and output

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,15 @@
+#include <stddef.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#ifndef _PUTCHAR
+#define _PUTCHAR
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+#endif /*_PUTCHAR*/
