3790b66e718bdf1fee82789b106b357d9d07a3af
gom6s <gmensah025@gmail.com>
2022-11-04 16:13:52 +0000

    File input and output

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,23 +8,18 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file, i = 0;
+	int file, w, i = 0;
 
 	if (filename == NULL)
-		return (-1);
-
-	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (file == -1)
 		return (-1);
 
 	while (text_content[i])
 		i++;
 
-	if (text_content != NULL)
-	{
-		write(file, text_content, i);
-	}
-	if (file == -1)
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(file, text_content, i);
+
+	if (file == -1 || w == -1)
 		return (-1);
 
 	close(file);
